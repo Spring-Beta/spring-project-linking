a7f1728629298ea6db2635cb76e6cccfb22d5dda
MD ALIF BABU <alif@k53tech.com>
2025-01-21 16:26:04 +0600

    initial commit

---
+++ src/main/java/com/example/springprojectlinking/SpringProjectLinkingApplication.java
@@ -0,0 +1,13 @@
+package com.example.springprojectlinking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectLinkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProjectLinkingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springprojectlinking/SpringProjectLinkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springprojectlinking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectLinkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
